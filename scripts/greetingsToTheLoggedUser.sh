c3f4e1941e41f83e530ee2e0f9d58942f31f4aa9
Emanuele Picariello <emanuelepicariello97@gmail.com>
2022-05-02 05:26:10 -0400

    added a new script reboot.sh and updated a script

--- scripts/greetingsToTheLoggedUser.sh
+++ scripts/greetingsToTheLoggedUser.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#This script if you run it, will output a greeting to the current user
 user=$(whoami)
 echo "Hello $user"
---
+++ scripts/reboot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This script if you run it will reboot your computer
+echo "The system will be rebooted in a few second ..."
+systemctl reboot
